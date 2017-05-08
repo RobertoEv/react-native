fc89a5a4b55ea045ba36178d5c3d7a78f27c41ab
Roberto Evangelista <Roberto Evangelista>
2017-05-08 19:03:33 -0300

    Adicionando componentes no app7

---
+++ app7/src/components/Operacao.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Picker, StyleSheet } from 'react-native';
+
+export default class Operacao extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { operacao: '' };
+  }
+
+  render() {
+    return (
+      <Picker
+        style={styles.operacao}
+        selectedValue={this.state.operacao}
+        onValueChange={operacao => { this.setState({ operacao }); }}
+      >
+        <Picker.Item label='Soma' value='soma' />
+        <Picker.Item label='Subtrair' value='subtrair' />
+      </Picker>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  operacao: {
+    marginTop: 15,
+    marginBottom: 15
+  }
+});
